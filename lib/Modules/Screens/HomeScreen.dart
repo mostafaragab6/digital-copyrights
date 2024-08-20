13ba3a56e9009d42ba1dc09b04ec44c1dec49aa7
Mostafa Ragab <moragab567@yahoo.com>
2024-08-20 20:44:38 +0300

    fake push

--- lib/Modules/Screens/HomeScreen.dart
+++ lib/Modules/Screens/HomeScreen.dart
@@ -5,26 +5,21 @@ import '../../Models/AllPostsModel.dart';
 import '../../Models/AppCubit/AppCubit.dart';
 import '../../Models/AppCubit/AppStates.dart';
 
-
-
 class HomeScreen extends StatelessWidget {
-
-
   DateTime dateTime = DateTime.now();
-
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AppCubit,AppStates>(
+    return BlocConsumer<AppCubit, AppStates>(
       builder: (BuildContext context, AppStates state) {
         var cubit = AppCubit.ACubit(context);
         return RefreshIndicator(
           onRefresh: () async {
             await Future.delayed(Duration(seconds: 1));
-           cubit.GetProfileData();
-           cubit.GetPostsAllData();
-           cubit.GetNotification();
-           cubit.GetUserPostsData();
+            cubit.GetProfileData();
+            cubit.GetPostsAllData();
+            cubit.GetNotification();
+            cubit.GetUserPostsData();
           },
           child: Stack(
             alignment: Alignment.topLeft,
@@ -34,48 +29,44 @@ class HomeScreen extends StatelessWidget {
                 width: double.infinity,
                 decoration: BoxDecoration(
                     gradient: LinearGradient(
-                        transform:GradientRotation(44) ,
-                        begin:Alignment.topRight ,
+                        transform: GradientRotation(44),
+                        begin: Alignment.topRight,
                         end: Alignment.bottomLeft,
                         colors: [
-                          MaterialColor(0xFF1B0810, {}),
+                      MaterialColor(0xFF1B0810, {}),
 
-                          Colors.black,
-                          //Colors.black,
+                      Colors.black,
+                      //Colors.black,
 
-                          MaterialColor(0xFF021222, {})
-                        ])),
+                      MaterialColor(0xFF021222, {})
+                    ])),
               ),
               Padding(
-                padding: const EdgeInsetsDirectional.only(top: 10.0,start: 10.0,end: 10.0 ),
+                padding: const EdgeInsetsDirectional.only(
+                    top: 10.0, start: 10.0, end: 10.0),
                 child: Row(
                   children: [
-
-                    Text('Good Morning, ',
+                    Text(
+                      'Good Morning, ',
                       style: TextStyle(
-                          decoration:TextDecoration.none,
+                          decoration: TextDecoration.none,
                           fontSize: 18.0,
                           fontWeight: FontWeight.w600,
-                          color: Colors.grey[300]
-                      ),),
+                          color: Colors.grey[300]),
+                    ),
                     Spacer(),
-
                   ],
                 ),
               ),
-
-
               Padding(
-                padding: const EdgeInsetsDirectional.only(top:50.0),
+                padding: const EdgeInsetsDirectional.only(top: 50.0),
                 child: ConditionalBuilder(
                   condition: cubit.postsAllData != null,
                   builder: (BuildContext context) {
                     return ListView.separated(
-
-                        itemBuilder: (context , index){
-
-                          String dateTimeString = cubit.postsAllData!.content[index].created_at!;
-
+                        itemBuilder: (context, index) {
+                          String dateTimeString =
+                              cubit.postsAllData!.content[index].created_at!;
 
                           String year = dateTimeString.substring(0, 4);
                           String month = dateTimeString.substring(5, 7);
@@ -84,182 +75,220 @@ class HomeScreen extends StatelessWidget {
                           String minute = dateTimeString.substring(14, 16);
                           return Column(
                             children: [
-                              cubit.flag? Center(child: CircularProgressIndicator()):SizedBox(),
-                              cubit.flag?SizedBox(height: 10.0,):SizedBox(),
+                              cubit.flag
+                                  ? Center(child: CircularProgressIndicator())
+                                  : SizedBox(),
+                              cubit.flag
+                                  ? SizedBox(
+                                      height: 10.0,
+                                    )
+                                  : SizedBox(),
                               CreateItem(
-                                cubit: cubit,
-                                postsAllContent: cubit.postsAllData!.content[index],
-                                dateTime: dateTime,
-                                day: day,
-                                hour: hour,
-                                minute: minute,
-                                month: month,
-                                  year: year
-                              ),
+                                  cubit: cubit,
+                                  postsAllContent:
+                                      cubit.postsAllData!.content[index],
+                                  dateTime: dateTime,
+                                  day: day,
+                                  hour: hour,
+                                  minute: minute,
+                                  month: month,
+                                  year: year),
                             ],
                           );
                         },
-                        separatorBuilder: (context , index)=> Padding(
-                          padding: const EdgeInsetsDirectional.symmetric(horizontal: 20.0),
-                          child: Container(
-                            width: double.infinity,
-                            height: 1.0,
-                            color: Colors.grey[800],
-                          ),
-                        ),
+                        separatorBuilder: (context, index) => Padding(
+                              padding: const EdgeInsetsDirectional.symmetric(
+                                  horizontal: 20.0),
+                              child: Container(
+                                width: double.infinity,
+                                height: 1.0,
+                                color: Colors.grey[800],
+                              ),
+                            ),
                         itemCount: cubit.postsAllData!.content.length);
                   },
                   fallback: (BuildContext context) {
                     return Center(
-                      child: CircularProgressIndicator()
-                      ,);
+                      child: CircularProgressIndicator(),
+                    );
                   },
-
                 ),
               ),
             ],
           ),
         );
-
       },
-      listener: (BuildContext context, AppStates state) {  },
-
+      listener: (BuildContext context, AppStates state) {},
     );
   }
 }
+
 Widget CreateItem(
-    {
-        required PostsAllContent postsAllContent,
-      required AppCubit cubit,
-      required  String year,
-      required  String month,
-      required  String day,
-      required  String hour,
-      required  String minute,
-      required  DateTime dateTime})=> Container(
-
-  child:
-  Padding(
-    padding: const EdgeInsets.all(10.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsetsDirectional.only(bottom: 10.0),
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 19.0,
-<<<<<<< HEAD
-                backgroundImage:
-                AssetImage('images/user.png')
-
-=======
-                backgroundImage: postsAllContent.userPostInfo!.image_url == null || postsAllContent.userPostInfo == null ?
-                AssetImage('images/user.png'):
-                NetworkImage('${postsAllContent.userPostInfo!.image_url}')
->>>>>>> 4c68008072001c35f36edcd57660f7132465f9b0
-                ,),
-              SizedBox(width: 15.0,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                       postsAllContent.userPostInfo != null ?Text('${postsAllContent.userPostInfo!.first_name} ${postsAllContent.userPostInfo!.last_name}',
-                        style: TextStyle(
-                            decoration:TextDecoration.none,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.grey[300]
-                        ),):Text('Unknown User',
-                         style: TextStyle(
-                             decoration:TextDecoration.none,
-                             fontSize: 15.0,
-                             fontWeight: FontWeight.w600,
-                             color: Colors.grey[300]
-                         ),),
-                      SizedBox(width: 5.0,),
-                      Icon(Icons.check_circle, color: Colors.blue,size: 19.0,),
-                    ],
-                  ),
-
-                  //15:02:20
-                  //2024-06-22T15:02:20
-                  dateTime.month - int.parse(month) != 0 ? Text('${dateTime.month - int.parse(month)}mon',style: TextStyle(color: Colors.grey[200]),):
-                  dateTime.month - int.parse(month) == 0 && dateTime.day - int.parse(day) != 0 ? Text('${dateTime.day - int.parse(day)}d',style: TextStyle(color: Colors.grey[200])):
-                  dateTime.month - int.parse(month) == 0 && dateTime.day - int.parse(day) == 0 && dateTime.hour - int.parse(hour) != 0 ? Text('${dateTime.hour - int.parse(hour)}h',style: TextStyle(color: Colors.grey[200])):
-                  Text('${dateTime.minute - int.parse(minute)}min',style: TextStyle(color: Colors.grey[200]))
-                  ,
-                ],
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsetsDirectional.only(end: 15.0),
-                child: Icon(Icons.keyboard_control,color: Colors.white,),
-              )
-
-            ],
-          ),
-        ),
-
-
-
-        postsAllContent.title != null && (postsAllContent.image_url == null || postsAllContent.image_url!.endsWith("media/")) ?Text('${postsAllContent.title}',
-          style:
-          TextStyle(
-              height: 2.0,
-              fontSize: 17.0,
-              fontWeight: FontWeight.w400,
-              color: Colors.white,
-              decoration: TextDecoration.none
-          ),)
-            : postsAllContent.title == null && postsAllContent.image_url != null ?  Padding(
-          padding: const EdgeInsetsDirectional.only(top: 8.0),
-          child: Container(
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Image(image: NetworkImage('${postsAllContent.image_url}'))),
-          ),
-        ) : Column(
+        {required PostsAllContent postsAllContent,
+        required AppCubit cubit,
+        required String year,
+        required String month,
+        required String day,
+        required String hour,
+        required String minute,
+        required DateTime dateTime}) =>
+    Container(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('${postsAllContent.title}',
-              style:
-              TextStyle(
-                  height: 2.0,
-                  fontSize: 17.0,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                  decoration: TextDecoration.none
-              ),),
-            SizedBox(height: 10.0,),
-
             Padding(
-              padding: const EdgeInsetsDirectional.only(top: 8.0),
-              child: Container(
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: Image(image: NetworkImage('${postsAllContent.image_url}'))),
+              padding: const EdgeInsetsDirectional.only(bottom: 10.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 19.0,
+                    backgroundImage: AssetImage('images/user.png'),
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          postsAllContent.userPostInfo != null
+                              ? Text(
+                                  '${postsAllContent.userPostInfo!.first_name} ${postsAllContent.userPostInfo!.last_name}',
+                                  style: TextStyle(
+                                      decoration: TextDecoration.none,
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.grey[300]),
+                                )
+                              : Text(
+                                  'Unknown User',
+                                  style: TextStyle(
+                                      decoration: TextDecoration.none,
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.grey[300]),
+                                ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Icon(
+                            Icons.check_circle,
+                            color: Colors.blue,
+                            size: 19.0,
+                          ),
+                        ],
+                      ),
+
+                      //15:02:20
+                      //2024-06-22T15:02:20
+                      dateTime.month - int.parse(month) != 0
+                          ? Text(
+                              '${dateTime.month - int.parse(month)}mon',
+                              style: TextStyle(color: Colors.grey[200]),
+                            )
+                          : dateTime.month - int.parse(month) == 0 &&
+                                  dateTime.day - int.parse(day) != 0
+                              ? Text('${dateTime.day - int.parse(day)}d',
+                                  style: TextStyle(color: Colors.grey[200]))
+                              : dateTime.month - int.parse(month) == 0 &&
+                                      dateTime.day - int.parse(day) == 0 &&
+                                      dateTime.hour - int.parse(hour) != 0
+                                  ? Text('${dateTime.hour - int.parse(hour)}h',
+                                      style: TextStyle(color: Colors.grey[200]))
+                                  : Text(
+                                      '${dateTime.minute - int.parse(minute)}min',
+                                      style:
+                                          TextStyle(color: Colors.grey[200])),
+                    ],
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 15.0),
+                    child: Icon(
+                      Icons.keyboard_control,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
               ),
-            )
+            ),
+            postsAllContent.title != null &&
+                    (postsAllContent.image_url == null ||
+                        postsAllContent.image_url!.endsWith("media/"))
+                ? Text(
+                    '${postsAllContent.title}',
+                    style: TextStyle(
+                        height: 2.0,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                        decoration: TextDecoration.none),
+                  )
+                : postsAllContent.title == null &&
+                        postsAllContent.image_url != null
+                    ? Padding(
+                        padding: const EdgeInsetsDirectional.only(top: 8.0),
+                        child: Container(
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: Image(
+                                  image: NetworkImage(
+                                      '${postsAllContent.image_url}'))),
+                        ),
+                      )
+                    : Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${postsAllContent.title}',
+                            style: TextStyle(
+                                height: 2.0,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                                decoration: TextDecoration.none),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsetsDirectional.only(top: 8.0),
+                            child: Container(
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Image(
+                                      image: NetworkImage(
+                                          '${postsAllContent.image_url}'))),
+                            ),
+                          )
+                        ],
+                      ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(
+                  top: 40.0, bottom: 10.0, start: 20.0, end: 20.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.favorite_outline,
+                    color: Colors.white,
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.question_answer_rounded,
+                    color: Colors.white,
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.reply_rounded,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
           ],
         ),
-
-
-        Padding(
-          padding: const EdgeInsetsDirectional.only(top: 40.0, bottom: 10.0,start: 20.0,end: 20.0),
-          child: Row(
-            children: [
-              Icon(Icons.favorite_outline,color: Colors.white,),
-              Spacer(),
-              Icon(Icons.question_answer_rounded,color: Colors.white,),
-              Spacer(),
-              Icon(Icons.reply_rounded,color: Colors.white,)
-            ],
-          ),
-        ),
-      ],
-    ),
-  ),
-);
+      ),
+    );
